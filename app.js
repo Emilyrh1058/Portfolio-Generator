a7c834ec47006d84e621293f6e261f33512ef82e
Emily Salinas <emilyrh1058@gmail.com>
2021-04-11 23:08:59 -0500

    updated generateProjects function

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
+const fs = require('fs');
 const inquirer = require("inquirer");
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage (name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -66,6 +58,7 @@ Add a New Project
 =================
 `);
 
+// if there isn't a 'projects' array property, create one
 if (!portfolioData.projects){
   portfolioData.projects = [];
 }
@@ -73,12 +66,28 @@ if (!portfolioData.projects){
     {
       type: "input",
       name: "name",
-      message: "What is the name of your project?",
+      message: "What is the name of your project? (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project name!');
+          return false;
+        }
+      }
     },
     {
       type: "input",
       name: "description",
       message: "Provide a description of the project (Required)",
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project description!');
+          return false;
+        }
+      }
     },
     {
       type: "checkbox",
@@ -133,6 +142,11 @@ if (!portfolioData.projects){
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
-})
+  const pageHTML = generatePage(portfolioData);
 
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  })
